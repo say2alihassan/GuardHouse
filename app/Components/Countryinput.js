744305b81d764a6e79642bc6b3be66af9b81168e
say2alihassan <ali.hassan.sulehree@gmail.com>
2022-12-15 23:02:56 +0500

    fixes/countrycodefactoring

--- app/Components/Countryinput.js
+++ app/Components/Countryinput.js
@@ -1,57 +1,112 @@
-import {StyleSheet, Text, View, TextInput, Dimensions} from 'react-native';
-import React, {useState, useRef} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Dimensions,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import React, {useState, useRef, useEffect} from 'react';
+import {DOWN} from '../assets/images';
 import CountryPicker from 'react-native-country-picker-modal';
 import {FONTS} from '../Style/font';
 
+import {PhoneInputState} from 'react-native-phone-number-input';
 import PhoneInput from 'react-native-phone-number-input';
 const Countryinput = () => {
   const phoneInput = useRef(null);
   console.log(phoneInput);
+  useEffect(() => {
+    console.log(phoneInput?.current?.getCallingCode());
+  }, []);
   const [selectedCallingCode, setSelectedCallingCode] = useState('90');
+  function pressCountry() {
+    console.log(PhoneInputState, 'STATE');
+    phoneInput?.current?.modalVisible === true;
+  }
+  function Render() {
+    return (
+      <View style={{position: 'absolute', right: -30, zIndex: -1}}>
+        <Text>ksdjfk</Text>
+      </View>
+    );
+  }
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <PhoneInput
-        placeholder="Type here....... "
-        // layout="second"
-        // defaultValue="0993833"
-        containerStyle={[
-          {
-            backgroundColor: '#fff',
+      <View>
+        <Image
+          source={DOWN}
+          style={{
+            height: 10,
+            width: 10,
+            position: 'absolute',
+            zIndex: 1,
+            top: 26,
+            left: 114,
+          }}
+          resizeMode={'contain'}
+        />
+        {/* <TouchableOpacity
+          onPress={() => pressCountry()}
+          style={{
+            position: 'absolute',
+            // top: 20,
+            zIndex: 1,
+            // backgroundColor
             height: 60,
-            width: 300,
+            width: 120,
+            backgroundColor: 'transparent',
             borderRadius: 20,
+            borderWidth: 1,
             borderColor: '#000',
-            borderWidth: 2,
-            paddingHorizontal: 5,
-            // paddingVertical: 5,
-            overflow: 'hidden',
-          },
-        ]}
-        // disableArrowIcon
-        textInputStyle={{
-          position: 'absolute',
-          top: -25,
-          // alignSelf: 'center',
-          color: '#000',
-          // backgroundColor: '#000',
-          borderLeftWidth: 1,
-          borderLeftColor: '#000',
-          fontFamily: FONTS.MEDIUM,
-          fontSize: 16,
-          // marginTop: -10,
-          // top: 3,
-          bottom: -5,
-          left: 60,
-          // marginLeft: 30,
-          width: Dimensions.get('screen').width,
-          height: 110,
-        }}
-        countryPickerButtonStyle={{
-          backgroundColor: '#fff',
-          width: 50,
-          // position: 'absolute',
-        }}
-      />
+          }}></TouchableOpacity> */}
+        <PhoneInput
+          placeholder="Type here....... "
+          // layout="second"
+          // defaultValue="0993833"
+          containerStyle={[
+            {
+              backgroundColor: '#fff',
+              height: 60,
+              width: 350,
+              borderRadius: 20,
+              borderColor: '#000',
+              borderWidth: 2,
+              paddingHorizontal: 5,
+              // paddingVertical: 5,
+              overflow: 'hidden',
+            },
+          ]}
+          disableArrowIcon
+          textInputStyle={{
+            position: 'absolute',
+            top: -25,
+            // alignSelf: 'center',
+            color: '#000',
+            // backgroundColor: '#000',
+            borderLeftWidth: 1,
+            borderLeftColor: '#000',
+            fontFamily: FONTS.MEDIUM,
+            fontSize: 16,
+            // marginTop: -10,
+            // top: 3,
+            bottom: -5,
+            left: 60,
+            // marginLeft: 30,
+            width: Dimensions.get('screen').width / 2,
+            height: 110,
+          }}
+          countryPickerButtonStyle={{
+            backgroundColor: 'transparent',
+            width: 60,
+            // position: 'absolute',
+            // right: -20,
+            // left: -50,
+          }}
+          // renderDropdownImage={Render()}
+        />
+      </View>
     </View>
   );
 };
